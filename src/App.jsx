0723df60bb232e52d6e7cc6246b8c83340609263
leolm83 <leonardo.mateus01@hotmail.com>
2024-04-18 13:46:30 -0300

    inicio do projeto

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Card from "./componets/card/Card";
+import Food from "./componets/food/Food";
+import Footer from "./componets/footer/Footer";
+import Header from "./componets/header/Header";
+function App() {
+  return (
+    <>
+    <Header/>
+    <h1>Hello!</h1>
+    <Card></Card>
+    <Food/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App;
---
+++ src/componets/card/Card.jsx
@@ -0,0 +1,10 @@
+function Card(){
+    return(
+        <div className="card">
+            <img alt="profile picture" src="https://via.placeholder.com/150"></img>
+            <h2>Leolm</h2>
+            <p>Software Engineer</p>
+        </div>
+    );
+}
+export default Card;
